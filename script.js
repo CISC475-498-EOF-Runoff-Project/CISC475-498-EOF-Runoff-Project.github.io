2a2510b6298eafec96efcc899c6df7c0d13a6009
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-10-13 15:03:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function pdfView(sw) {
     var src;
     if (sw == 0) {
-      src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/test1.pdf"
+      src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day0.pdf"
+  } else if (sw == 1) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.pdf"
+  } else if (sw == 2) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day2.pdf"
   } else if (sw == 3) {
-       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Day3.pdf"
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day3.pdf"
+  } else if (sw == 4) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day4.pdf"
+  } else if (sw == 5) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day5.pdf"
   } else {
     src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/event.pdf"
   }
