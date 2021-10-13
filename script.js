843a8755d1bc7c9cd2eff77e0e326edec26346bf
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-10-13 13:57:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function pdfView(sw) {
     if (sw == 0) {
       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/test1.pdf"
   } else if (sw == 3) {
-       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Day3.jfif"
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Day3.pdf"
   } else {
     src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/event.pdf"
   }
