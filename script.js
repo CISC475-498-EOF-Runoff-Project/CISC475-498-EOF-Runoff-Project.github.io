500033f51efe86cd5e969009e21477b07886e295
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-10-25 13:59:36 -0400

    handle day 8 9 10 tab clicks

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function pdfView(sw) {
        src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day4.pdf"
   } else if (sw == 5) {
        src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day5.pdf"
+  } 
+     else if (sw == 6) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day6.pdf"
+  } 
+     else if (sw == 7) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day7.pdf"
+  } 
+     else if (sw == 8) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day8.pdf"
+  } 
+     else if (sw == 9) {
+       src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day9.pdf"
   } else {
     src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/event.pdf"
   }
