7a2e4ea6410324a3bc0ed3cbbe4555103a12beb9
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-10-29 00:32:11 -0400

    put image with slidebar on top row

--- slidebar.js
+++ slidebar.js
@@ -1,12 +1,15 @@
 var val = document.getElementById("valR").value;
         document.getElementById("range").innerHTML=val;
         document.getElementById("img").src = val + ".jpg";
+
         function showVal(newVal){
           document.getElementById("range").innerHTML=newVal;
           if(newVal == 1)
-            document.getElementById("img").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
+                  
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
           else if(newVal == 2)
-            document.getElementById("img").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/blob/main/images/homogeneouscoords.jpg?raw=true";
+                  <li class = "navigation-item"><a class = "navigation-item-a" href="#Tomorrow" onclick="pdfView(1)">Tomorrow</a></li>
+            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/blob/main/images/homogeneouscoords.jpg?raw=true";
           else if(newVal == 3)
-            document.getElementById("img").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
         }
