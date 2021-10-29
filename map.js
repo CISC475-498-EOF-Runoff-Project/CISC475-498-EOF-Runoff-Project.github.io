b9a0454547e878f2a79a4a0044c278bd190f8960
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-10-29 13:08:04 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 var testData = {
   max: 8,
-  data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, ...]
+  data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, {lat: 29.6408, lng:46.7728, count: 3},]
 };
 
 var baseLayer = L.tileLayer(
@@ -39,7 +39,7 @@ var map = new L.Map('map-canvas', {
 });
 
 heatmapLayer.setData(testData);
-
+/*
 var mymap = L.map('mapid').setView([47.00, -87.00], 5);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmRlYW4iLCJhIjoiY2t1eWl3dnA2NzNpNTJwbzNvcHRxejdxaCJ9.tIGjuwey9icme7TC-y-U9g', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -49,8 +49,8 @@ var mymap = L.map('mapid').setView([47.00, -87.00], 5);
           zoomOffset: -1,
           accessToken: 'pk.eyJ1IjoicmRlYW4iLCJhIjoiY2t1eWl3dnA2NzNpNTJwbzNvcHRxejdxaCJ9.tIGjuwey9icme7TC-y-U9g'
         }).addTo(mymap);
-        heatmapLayer.addTo(mymap);
         var imageUrl = 'https://CISC475-498-EOF-Runoff-Project.github.io/images/Event_clear.png',
             imageBounds = [[51.2 + 2.0, -100.0 - 2.0], [35.0 - 2.0, -70.0 + 2.0]],
             imageOverlay = L.imageOverlay(imageUrl, imageBounds).addTo(mymap);
         imageOverlay.setOpacity(0.5);
+*/
