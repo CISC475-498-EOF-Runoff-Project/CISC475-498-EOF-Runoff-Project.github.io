ce475d4da0696d8a83a8e1e99be238302c3b8ba6
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-10-29 00:33:16 -0400

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -8,8 +8,8 @@ var val = document.getElementById("valR").value;
                   
             document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
           else if(newVal == 2)
-                  <li class = "navigation-item"><a class = "navigation-item-a" href="#Tomorrow" onclick="pdfView(1)">Tomorrow</a></li>
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/blob/main/images/homogeneouscoords.jpg?raw=true";
+                 
+            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.jpg";
           else if(newVal == 3)
-            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day3.jpg";
         }
