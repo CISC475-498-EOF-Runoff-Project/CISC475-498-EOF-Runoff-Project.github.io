e17a0f97cd683276e1a84ca47808090f31b2477c
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-10-29 00:37:40 -0400

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -4,12 +4,26 @@ var val = document.getElementById("valR").value;
 
         function showVal(newVal){
           document.getElementById("range").innerHTML=newVal;
-          if(newVal == 1)
-                  
-            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";
-          else if(newVal == 2)
-                 
+                
+          if(newVal == 1)               
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";            
+          else if(newVal == 2)                
             document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.jpg";
           else if(newVal == 3)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day2.jpg
+          else if(newVal == 4)
             document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day3.jpg";
+          else if(newVal == 5)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day4.jpg";           
+          else if(newVal == 6)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day5.jpg";
+          else if(newVal == 7)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day6.jpg";
+          else if(newVal == 8)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day7.jpg"
+          else if(newVal == 9)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day8.jpg";
+          else if(newVal == 10)
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day9.jpg";
+                
         }
