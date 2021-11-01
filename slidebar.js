50f0d7b35acb8364e8fce5520676135dd8ade7fb
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-11-01 14:53:09 -0400

    fix broken image file paths

--- slidebar.js
+++ slidebar.js
@@ -6,24 +6,24 @@ var val = document.getElementById("valR").value;
           document.getElementById("range").innerHTML=newVal;
                 
           if(newVal == 1)               
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.jpg";            
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_diff.pdf";            
           else if(newVal == 2)                
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.pdf";
           else if(newVal == 3)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day2.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day2.pdf";
           else if(newVal == 4)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/imagess/Event0_day3.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day3.pdf";
           else if(newVal == 5)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day4.jpg";           
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day4.pdf";           
           else if(newVal == 6)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day5.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day5.pdf";
           else if(newVal == 7)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day6.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day6.pdf";
           else if(newVal == 8)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day7.jpg"
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day7.pdf"
           else if(newVal == 9)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day8.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day8.pdf";
           else if(newVal == 10)
-            document.getElementById("plugin").src = "https://github.com/CISC475-498-EOF-Runoff-Project/CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day9.jpg";
+            document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day9.pdf";
                 
         }
