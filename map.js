6921aad12efa81ef43d007ba38dd455657af1ad3
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-11-05 14:45:04 -0400

    Update map.js

--- map.js
+++ map.js
@@ -19,8 +19,8 @@ const database = getDatabase(app);
 
 //import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";
 const databaseRef = ref(database);
-function pullFromFirebase() {
-  get(child(databaseRef, '1')).then((snapshot) => {
+function pullFromFirebase(id) {
+  get(child(databaseRef, id)).then((snapshot) => {
     if (snapshot.exists()) {
       console.log(snapshot.val());
     } else {
@@ -30,7 +30,7 @@ function pullFromFirebase() {
     console.error(error);
   });
 }
-pullFromFirebase();
+pullFromFirebase(1);
 
 var data;
 Papa.parse('./test_meters.csv', {
