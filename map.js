c02713821550576a35b3e57de40330f2a13f6183
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-11-10 14:53:31 -0500

    Update map.js

--- map.js
+++ map.js
@@ -34,6 +34,7 @@ function pullFromFirebase(id) {
 pullFromFirebase('1');
 
 var temp_data = pullFromFirebase('1');
+console.log(temp_data);
 var data = [];
 let temp_obj = {lat:temp_data.lat, lng:temp_data.lon, value:temp_data.intensity};
 data.push(temp_obj);
