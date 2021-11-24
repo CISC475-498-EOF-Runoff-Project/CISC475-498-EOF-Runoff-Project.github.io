1e4257bed1e09f48fc47bdc9cf2f407b89af7ac3
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-11-24 12:54:50 -0500

    Update map.js

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ var imageUrl = 'https://CISC475-498-EOF-Runoff-Project.github.io/images/Event_cl
 var imageUrl = 'https://CISC475-498-EOF-Runoff-Project.github.io/images/Event_clear.png';
 window.imageOverlay = L.imageOverlay(
     imageUrl,
-    [[51.2 + 3.6 + 0.9, -100.0 - 1.9 - 0.5], [35.0 - 5.5, -70.0 + 4.4]],
+    [[51.2 + 3.6 + 0.9, -100.0 - 1.9 - 0.5], [35.0 - 5.5, -70.0 + 4.5]],
     {
         opacity: 0.5,
         interactive: true
