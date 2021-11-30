df1cd71895550a14d6d2f409c31c593d2a5a7b93
ewallace99 <44401742+ewallace99@users.noreply.github.com>
2021-11-30 15:04:38 -0500

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -6,26 +6,28 @@ var val = document.getElementById("valR").value;
 //showVal() updates the (right now it is in pdf format) image of the runoff prediction based on where the user slides the slide bar
 
         function showVal(newVal){
+          const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+          
           document.getElementById("range").innerHTML=newVal;
           //call stat box display method      
           if(newVal == 1) {
             //window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event_clear.png");
             const d = new Date();
-            var dNoTime = d.getMonth()+' '+d.getDate()+', '+d.getFullYear();
+            var dNoTime = months[d.getMonth()]+' '+d.getDate()+', '+d.getFullYear();
         d.setDate(dNoTime);
           document.getElementById("show_time").innerHTML = d;
             window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_vars.png");
             //document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day0.pdf";            
            } else if(newVal == 2) {
             const d = new Date();
-            var dNoTime = d.getMonth()+' '+(d.getDate()+1)+', '+d.getFullYear();
+            var dNoTime = months[d.getMonth()]+' '+(d.getDate()+1)+', '+d.getFullYear();
         d.setDate(dNoTime);
           document.getElementById("show_time").innerHTML = d;
             window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event1_projected.png");
             //document.getElementById("plugin").src = "https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_day1.pdf";
            }else if(newVal == 3){
             const d = new Date();
-            var dNoTime = d.getMonth()+' '+(d.getDate()+2)+', '+d.getFullYear();
+            var dNoTime = months[d.getMonth()]+' '+(d.getDate()+2)+', '+d.getFullYear();
         d.setDate(dNoTime);
           document.getElementById("show_time").innerHTML = d;
             window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event2_projected.png");
