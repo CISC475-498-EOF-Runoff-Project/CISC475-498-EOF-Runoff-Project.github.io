60616043406a5afb4af39d1fdf3d85016f8fc10c
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-12-01 10:48:51 -0500

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -5,72 +5,77 @@ var val = document.getElementById("valR").value;
         
 //showVal() updates the (right now it is in pdf format) image of the runoff prediction based on where the user slides the slide bar
 
-        function showVal(newVal){
-          const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-          const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-          
-          document.getElementById("range").innerHTML=newVal;
-          var targetDate = new Date();
-          
-          //call stat box display method      
-          if(newVal == 1) {
-            targetDate.setDate(targetDate.getDate() + 0);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_vars.png");
-          } 
-          else if(newVal == 2) {
-            targetDate.setDate(targetDate.getDate() + 1);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event1_projected.png");
-          } 
-          else if(newVal == 3){
-            targetDate.setDate(targetDate.getDate() + 2);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event2_projected.png");
-          }
-          else if(newVal == 4) {
-            targetDate.setDate(targetDate.getDate() + 3);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event3_projected.png");
-          }
-          else if(newVal == 5) {
-            targetDate.setDate(targetDate.getDate() + 4);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event4_projected.png");
-          }
-          else if(newVal == 6) {
-            targetDate.setDate(targetDate.getDate() + 5);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event5_projected.png");
-          }
-          else if(newVal == 7) {
-            targetDate.setDate(targetDate.getDate() + 6);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event6_projected.png");
-          }
-          else if(newVal == 8) {
-            targetDate.setDate(targetDate.getDate() + 7);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event7_projected.png");
-          }
-          else if(newVal == 9) {
-            targetDate.setDate(targetDate.getDate() + 8);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event8_projected.png");
-          }
-          else if(newVal == 10) {
-            targetDate.setDate(targetDate.getDate() + 9);
-            var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
-            document.getElementById("show_time").innerHTML = date_to_show;
-            window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event9_projected.png");
-          }
-        }
+function showVal(newVal){
+  const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  document.getElementById("range").innerHTML=newVal;
+  var targetDate = new Date();
+
+  //call stat box display method      
+  if(newVal == 1) {
+    targetDate.setDate(targetDate.getDate() + 0);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_vars.png");
+    var table = document.getElementById("statsTable");
+    if (table.tBodies[0].rows.length >= newVal) {
+      table.tBodies[0].rows[newVal-1].style.color = #ffffff;
+    }
+
+  } 
+  else if(newVal == 2) {
+    targetDate.setDate(targetDate.getDate() + 1);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event1_projected.png");
+  } 
+  else if(newVal == 3){
+    targetDate.setDate(targetDate.getDate() + 2);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event2_projected.png");
+  }
+  else if(newVal == 4) {
+    targetDate.setDate(targetDate.getDate() + 3);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event3_projected.png");
+  }
+  else if(newVal == 5) {
+    targetDate.setDate(targetDate.getDate() + 4);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event4_projected.png");
+  }
+  else if(newVal == 6) {
+    targetDate.setDate(targetDate.getDate() + 5);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event5_projected.png");
+  }
+  else if(newVal == 7) {
+    targetDate.setDate(targetDate.getDate() + 6);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event6_projected.png");
+  }
+  else if(newVal == 8) {
+    targetDate.setDate(targetDate.getDate() + 7);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event7_projected.png");
+  }
+  else if(newVal == 9) {
+    targetDate.setDate(targetDate.getDate() + 8);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event8_projected.png");
+  }
+  else if(newVal == 10) {
+    targetDate.setDate(targetDate.getDate() + 9);
+    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    document.getElementById("show_time").innerHTML = date_to_show;
+    window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event9_projected.png");
+  }
+}
