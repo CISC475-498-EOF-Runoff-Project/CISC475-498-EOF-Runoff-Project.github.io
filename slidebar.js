e68876a2dfd0efbbfdf120e8f7406b715fddac29
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-12-03 14:57:57 -0500

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -1,6 +1,9 @@
 var val = document.getElementById("valR").value;
         document.getElementById("range").innerHTML=val;
-        
+
+// initialize to day 1
+showVal(1);
+
 //showVal() updates the (right now it is in pdf format) image of the runoff prediction based on where the user slides the slide bar
 
 function showVal(newVal) {
