414228352b45e53a6d713f2d2aac50fc0b66dbcd
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-12-06 10:37:57 -0500

    Update slidebar.js

--- slidebar.js
+++ slidebar.js
@@ -15,7 +15,7 @@ function showVal(newVal) {
   //call stat box display method   
   if(newVal == 1) {
     targetDate.setDate(targetDate.getDate() + 0);
-    var date_to_show = days[targetDate.getDay()] + ", " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
+    var date_to_show = "Today, " + months[targetDate.getMonth()] + " " + targetDate.getDate() + ", " + targetDate.getFullYear();
     document.getElementById("show_time").innerHTML = date_to_show;
     window.imageOverlay.setUrl("https://CISC475-498-EOF-Runoff-Project.github.io/images/Event0_projected.png");
   }
