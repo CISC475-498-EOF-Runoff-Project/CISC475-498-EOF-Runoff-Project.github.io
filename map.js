d6dbfb945813b6af92765b5c2aa2a12cc6555ede
Ryan Dean <79111850+rdean-udel@users.noreply.github.com>
2021-12-07 11:24:06 -0500

    Update map.js

--- map.js
+++ map.js
@@ -34,12 +34,12 @@ mymap.setMaxBounds([
     [52.73, -100.49],
     [30.13, -64.72]
 ]);
-//imageOverlay.bindPopup(imagePopup);
-
 
 /* make popup object to display when map is clicked */
 
 var popup = L.popup();
+
+/* create helper text, disappears once map is clicked */
 
 var statsTableHolder = document.getElementById("statsTableHolder");
 var helperSpan = document.createElement('span');
@@ -48,9 +48,8 @@ var helperText = document.createTextNode("Click a region on the map to get more 
 helperSpan.append(helperText);
 statsTableHolder.appendChild(helperSpan);
 
-/* function that runs when map is clicked. Adds popup and
-   puts 10 days of data into stats box */
-/*
+/* function that runs when map is clicked. Adds popup + fills table with 10 days of stats */
+
 function imagePopup(e) {
     //var e = leafletEvent.originalEvent;
     var temp_event = e.originalEvent;
@@ -60,120 +59,7 @@ function imagePopup(e) {
 
     const x = Math.round(zoomedX * imgWidth / rect.width);
     const y = Math.round(zoomedY * imgHeight / rect.height);
-        
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = imgWidth;
-    canvas.height = imgHeight;
-    
-    var data_10_days = [];    
-    for(var day = 0; day < 10; day++) {
-        
-        // set html element to correct image
-        let imgVars = new Image();
-        imgVars.src = 'https://CISC475-498-EOF-Runoff-Project.github.io/images/Event' + day + '_vars.png';
-        ctx.clearRect(0, 0, imgWidth, imgHeight);
-        ctx.drawImage(imgVars, 0, 0, imgWidth, imgHeight);
-        let varsData = ctx.getImageData(x, y, imgWidth, imgHeight); 
-        let accprcp = ((varsData.data[0] / 255) * 200).toFixed(2);
-        let acsnom = ((varsData.data[1] / 255) * 200).toFixed(2);
-        let qsnow = ((varsData.data[2] / 255) * 200).toFixed(2);
 
-        let imgRisk = new Image();
-        imgRisk.src = 'https://CISC475-498-EOF-Runoff-Project.github.io/images/Event' + day + '_projected.png';
-        
-        ctx.clearRect(0, 0, imgWidth, imgHeight);
-        ctx.drawImage(imgRisk, 0, 0, imgWidth, imgHeight);
-        let riskData = ctx.getImageData(x, y, imgWidth, imgHeight); 
-        let riskRed = riskData.data[0];
-        let riskGreen = riskData.data[1];
-        let riskBlue = riskData.data[2];
-        let daily_risk = "MINIMAL";
-        let max_risk_color = Math.max(riskData.data[0], riskData.data[1], riskData.data[2]);
-        if (riskRed == 0) {
-            daily_risk = "MINIMAL";
-        } else if (max_risk_color == riskRed) {
-            daily_risk = "HIGH";
-        } else if (max_risk_color == riskGreen) {
-            daily_risk = "LOW";
-        } else {
-            if (riskRed > riskGreen) {
-                daily_risk = "MODERATE";
-            }
-        }
-        
-        let formatted_day = new Date();
-        let date_to_show = "Today";
-        if (day != 0) {
-            formatted_day.setDate(formatted_day.getDate() + day);
-            date_to_show = (formatted_day.getMonth()+1) + "/" + formatted_day.getDate();
-        }
-        let data_by_day = [date_to_show, daily_risk, accprcp, acsnom, qsnow];
-        data_10_days[day] = data_by_day;
-        
-        //fillGridRow(x, y, day);
-    }
-    
-    var statsTable = document.getElementById("popupStatsTable");
-  
-    if (statsTable.tBodies[0].rows.length == 0) {
-        statsTableHolder.removeChild(helperSpan);
-    }
-    
-    //iterate through rows
-    for(var j = 0; j < 10; j++) {
-        if (statsTable.tBodies[0].rows.length < j+1) {
-            statsTable.tBodies[0].insertRow(j);
-        }
-        //iterate through cells in row
-        for (var k = 0; k < 5; k++) {
-            if (statsTable.tBodies[0].rows[j].cells.length < k+1) {
-                statsTable.tBodies[0].rows[j].insertCell(k);
-            }
-            if (data_10_days[j][k] == 0.00) {
-                statsTable.tBodies[0].rows[j].cells[k].innerHTML = "--";
-            }
-            else {
-                statsTable.tBodies[0].rows[j].cells[k].innerHTML = data_10_days[j][k];
-            }
-        }
-        if (data_10_days[j][1] == "MINIMAL") {
-            statsTable.tBodies[0].rows[j].cells[1].setAttribute("style","color: #BBFFBB");
-        }
-        else if (data_10_days[j][1] == "LOW") {
-            statsTable.tBodies[0].rows[j].cells[1].setAttribute("style","color: white");
-        }
-        else if (data_10_days[j][1] == "MODERATE") {
-            statsTable.tBodies[0].rows[j].cells[1].setAttribute("style","color: #FFDDBB");
-        }
-        else {
-            statsTable.tBodies[0].rows[j].cells[1].setAttribute("style","color: #FFBBBB");
-        }
-    }
-    
-    let str = window.imageOverlay.getElement().src;
-    let popupday = str.charAt(str.length - 15);
-    popup
-        .setLatLng(e.latlng)
-        .setContent('<H6>RISK: ' + statsTable.tBodies[0].rows[popupday].cells[1].innerHTML + '</H6>')
-        .openOn(mymap);   
-}
-*/
-function imagePopup(e) {
-    //var e = leafletEvent.originalEvent;
-    var temp_event = e.originalEvent;
-    var rect = temp_event.target.getBoundingClientRect();
-    var zoomedX = temp_event.clientX - rect.left; //x position within the element.
-    var zoomedY = temp_event.clientY - rect.top;  //y position within the element
-
-    const x = Math.round(zoomedX * imgWidth / rect.width);
-    const y = Math.round(zoomedY * imgHeight / rect.height);
-    /*    
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = imgWidth;
-    canvas.height = imgHeight;
-    */
     var statsTable = document.getElementById("popupStatsTable");
   
     if (statsTable.tBodies[0].rows.length == 0) {
@@ -192,6 +78,7 @@ function imagePopup(e) {
             }
         }
     }
+    
     let str = window.imageOverlay.getElement().src;
     let popupday = str.charAt(str.length - 15);
     
@@ -199,6 +86,10 @@ function imagePopup(e) {
         fillGridRow(x, y, day, popupday, e);
     }   
 }
+
+
+/* TODO: Change function name to updatePopups() */
+/* Helper function called in slidebar.js to update popup text */
 
 function clearpopups(newDay) {
     //mymap.closePopup();
@@ -208,11 +99,11 @@ function clearpopups(newDay) {
     }
 }
 
-//const imgWidth = 1500, imgHeight = 1200;
 const imgWidth = 1600, imgHeight = 1600;
 window.imageOverlay.on('click', imagePopup);
-//mymap.on('click', makePopup);
 
+
+/* Fills one row of the stats box table. Done independently because js loads images async */
 
 function fillGridRow(x, y, img_day, popupday, e) {
     
