e6fea5af82527ad7393e01a79bd11487211b7160
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2022-05-16 20:01:42 -0400

    added geoapify api to geocode input street address. returns an object containing coordinates in the console

---
+++ Javascript/searchbar.js
@@ -0,0 +1,20 @@
+//function for searchbar to parse the entered address and return latitude and longitude to the model for focus
+function geocodeAddress(){
+	var requestOptions = {
+		method: 'GET',
+  	};
+	const address = document.getElementById("addressInput").value;
+	if (!address || address.length < 3) {
+		alert("The address string is too short. Enter at least three symbols");
+		return;
+	}
+	
+	const geocodeURL = 'https://api.geoapify.com/v1/geocode/search?text=' + encodeURIComponent(address) + '&apiKey=209bb934353745d3ace852db14dc31a2';
+
+  	fetch(geocodeURL, requestOptions)
+		.then(response => response.json())
+		.then(result => console.log(result))
+		.catch(error => console.log('error', error));
+	  
+	//alert(result);
+	}
